32eb576f9f660a7b1a81abdc9003055230ce6b58
rachiderrami <errami.rachid@outlook.com>
2022-11-17 18:31:12 -0500

    landing page

---
+++ Desktop/webDev/CSS/tailwind/react_tailwind_website/tailwind/src/component/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Typed from 'react-typed';
+
+
+const Hero = () => {
+  return (
+    <div className='text-white'>
+      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+        <p className="text-[#00df9a] font-bold p-2 ">GROWING WITH DATA ANALYTICS</p>
+        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>grow with data</h1>
+
+
+      <div className='flex justify-center items-center '>
+        <p className="md:text-5xl sm:text-4xl text-xl font-bold pl-4 ">Fast , flexible financing for <Typed strings={['MATH','AND','FUN']} typeSpeed={120} backSpeed={140} loop /></p>
+      
+      </div>
+      <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB , BTC and SASS platforms</p>
+      <button className='bg-[#00df9a] w-[200px] rounded-md my-6 mx-auto  py-3 tect-black'>Get Started</button>
+      </div>
+    
+    
+    </div>
+
+  )
+}
+
+export default Hero
